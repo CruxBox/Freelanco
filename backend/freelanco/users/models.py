864440b2c0989ad6e6a9c541794078748db38a66
Singam Bhargav Ram <singambhargavram@gmail.com>
2020-03-25 18:54:07 +0530

    Add profile functionality

--- backend/freelanco/users/models.py
+++ backend/freelanco/users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from allauth.account.signals import user_signed_up
 
 LOCATION_CHOICES=[
     ('Hyd','Hyderabad'),
@@ -28,30 +29,23 @@ class CustomerProfile(models.Model):
     user=models.OneToOneField(to=CustomUser,on_delete=models.CASCADE,null=False,related_name="customer_profile")
     location=models.TextField(choices=LOCATION_CHOICES,max_length=None,default='Hyd')
     address=models.OneToOneField(to=Address,on_delete=models.CASCADE,null=True,related_name="customer_address")
-    image=models.ImageField(upload_to='profile_image',blank=True)
+    image=models.ImageField(upload_to='profile_image',blank=True,default="icon.png")
 
     #should add additional fields
 
     def __str__(self):
         return f"Customer-{self.user.username}"
 
+    #creates customer profile for user who signup using google account
+    @receiver(user_signed_up)
+    def populate_profile(sociallogin,user,**kwargs):
+        user.customer_profile=CustomerProfile()
+        #user.customer_profile.image=user.socialaccount_set.filter(provider='google')[0].extra_data['picture']
+        user.customer_profile.save()
 class FreelancerProfile(models.Model):
     user=models.OneToOneField(to=CustomUser,on_delete=models.CASCADE,null=False,related_name="freelancer_profile")
     location=models.TextField(choices=LOCATION_CHOICES,max_length=None,default='Hyd')
 
    #add additional fields
-
     def __str__(self):
         return f"Freelancer-{self.user.username}"
-
-
-"""
-@receiver(post_save, sender=CustomUser)
-def create_or_update_user(sender, instance,created,**kwargs):
-    if created:
-        print("Damne")
-        if instance.is_freelancer:
-            profile=FreelancerProfile.objects.create(user=instance)
-        else:
-            profile=CustomerProfile.objects.create(user=instance)
-"""
