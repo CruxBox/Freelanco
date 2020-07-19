c289eed0c7535193e16ce7e8951215f276ceffdc
Abhinav Talari <49162896+AbhinavTalari@users.noreply.github.com>
2020-07-20 02:19:30 +0530

    Dummy URL order_history and changes dummy path in the freelanco/urls.py

--- backend/freelanco/freelanco/urls.py
+++ backend/freelanco/freelanco/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from .views import gettingStarted,display404
+from .views import gettingStarted,display404,displayOrderHist
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,7 +11,10 @@ urlpatterns = [
 	path('home/', include('product.urls')),
 	path('accounts/',include('users.urls')),
 	path('accounts/',include('allauth.urls')),
-	path('notfound',display404)
+	path('notfound',display404),
+	
+  ##For Testing
+	path('order_hist',displayOrderHist)
  
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- backend/freelanco/freelanco/views.py
+++ backend/freelanco/freelanco/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 def gettingStarted(request):
     return render(request, "index.html",{})
 
+
+##for testing
 def display404(request):
     return render(request,"notfound.html",{})
+
+def displayOrderHist(request):
+    return render(request,"order_history.html")
